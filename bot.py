c602efa3363dde25f57a958abcfe9072e9c3a927
japon2027 <CodNeoz3+github@gmail.com>
2026-08-09 12:04:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from zoneinfo import ZoneInfo
 TOKEN = os.environ["TOKEN"]
 CHANNEL_ID = int(os.environ["CHANNEL_ID"])
 CIBLE = datetime(2027, 4, 25, 12, 0, tzinfo=ZoneInfo("Europe/Brussels"))
-HEURE_ENVOI = time(hour=12, minute=00, tzinfo=ZoneInfo("Europe/Brussels"))
+HEURE_ENVOI = time(hour=12, minute=6, tzinfo=ZoneInfo("Europe/Brussels"))
 DOSSIER_IMGS = "imgs"                    # dossier des images dans le repo
 EXT_IMAGES = (".png", ".jpg", ".jpeg", ".gif", ".webp")
 
